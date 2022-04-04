361831e02afa6be3e8a78b685710b9fc401beebc
Racing Panda <44290569+rVSaxena@users.noreply.github.com>
2022-04-05 01:34:04 +0530

    prepared constructs for gmb dataset

--- constructs.py
+++ constructs.py
@@ -5,8 +5,10 @@ Write additional code that saves a vocabulary, and converts the dataset into int
 """
 
 import torch
+import pandas as pd
+import numpy as np
 from torch import nn
-from torch.nn.utils.rnn import pad_packed_sequence
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
 class LangModel(nn.Module):
 
@@ -21,6 +23,7 @@ class LangModel(nn.Module):
         super(LangModel, self).__init__()
         self.batch_first=batch_first
         self.embed_dim=embed_dim
+        self.num_classes=num_classes
         self.lstm=nn.LSTM(
             input_size=embed_dim,
             hidden_size=128,
@@ -43,54 +46,64 @@ class LangModel(nn.Module):
         else (max_seq_len, N, embed_dim)
         """
 
-        tmp_output, _, _=self.lstm(x)
-        tmp_output=pad_packed_sequence(tmp_output, batch_first=self.batch_first)
-        a, b=tmp_output[:, :, :self.embed_dim], tmp_output[:, :, self.embed_dim]
+        tmp_output, _=self.lstm(x)
+        tmp_output, tmp_batch_sizes=pad_packed_sequence(tmp_output, batch_first=self.batch_first)
+        a, b=tmp_output[:, :, :self.num_classes], tmp_output[:, :, self.num_classes:]
 
         if self.use_softmax:
-            return self.softmax(a+b)
+            return pack_padded_sequence(self.softmax(a+b), tmp_batch_sizes, batch_first=self.batch_first, enforce_sorted=False)
         
-        return a+b
+        return pack_padded_sequence(a+b, tmp_batch_sizes, batch_first=self.batch_first, enforce_sorted=False)
 
 class ner_dataset(torch.utils.data.Dataset):
 
-    def __init__(self, dataset_loc, vocab_size, max_seq_len=40):
+    def __init__(self, dataset_loc, target_loc, vocab_size, max_seq_len=40):
         
         super(ner_dataset, self).__init__()
         
-        if vocab_size>65536:
-            tmp_data=pd.read_csv(dataset_loc, header=None, dtype=np.uint32).values
-        else:
-            tmp_data=pd.read_csv(dataset_loc, header=None, dtype=np.uint16).values
+        unpad_x=pd.read_csv(dataset_loc).values
+        target=pd.read_csv(target_loc).values
 
-        unpad_x, target=tmp_data[:, :-1], tmp_data[:, -1]
         batch_sizes=unpad_x.shape[1]-np.sum(np.isnan(unpad_x), axis=1)
 
         # now, just convert the nan to 0 to pad
         # Note again, vocal must have index 0 reserved for pad
 
         unpad_x[np.isnan(unpad_x)]=0
+        target[np.isnan(target)]=100
 
-        self.unpad_x=torch.from_numpy(unpad_x)
-        self.ner_targets=torch.from_numpy(target)
-        self.batch_sizes=torch.from_numpy(batch_sizes)
+        self.pad_x=torch.from_numpy(unpad_x).to(torch.int32)
+        self.ner_targets=torch.from_numpy(target).to(torch.int32)
+        self.batch_sizes=torch.from_numpy(batch_sizes).to(torch.int32)
 
         return
 
     def __len__(self):
 
-        return len(self.unpad_x)
+        return len(self.pad_x)
 
     def __getitem__(self, idx):
 
-        return self.unpad_x[idx], self.batch_first[idx], self.ner_targets[idx]
+        return self.pad_x[idx], self.batch_sizes[idx], self.ner_targets[idx]
 
 args={}
 
-args{"logging_dir"}=""
-args{"epochs"}=30
+args["logging_dir"]="./NER_run1"
+args["target_pad_value"]=100
+args["epochs"]=30
 args["optimizer"]=lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 args["lr_schedulers"]=[lambda opt: torch.optim.lr_scheduler.StepLR(opt, 9, gamma=0.8)]
-args{"batch_first"}=True
-args{"device"}="cuda"
-args{"embedding_dim"}=50
+args["batch_first"]=True
+args["device"]="cuda"
+args["embedding_dim"]=50
+args["max_seq_len"]=104
+args["num_classes"]=17
+
+dataset=ner_dataset("finData/data.csv", "finData/labels.csv", 36038, args["max_seq_len"])
+trainloader=torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+
+lang_model=LangModel(args["embedding_dim"], args["num_classes"])
+
+args["trainLoader"]=trainloader
+args["lang_model"]=lang_model
+args["vocab_size"]=36038
