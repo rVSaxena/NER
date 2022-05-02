4e33551fb0238368cf9fefebabd185e1c0814a46
Racing Panda <44290569+rVSaxena@users.noreply.github.com>
2022-05-03 02:14:39 +0530

    adding vectorized forward_algo dp

--- CRF.py
+++ CRF.py
@@ -18,6 +18,7 @@ class CRF(nn.Module):
 		self.transition_scores=nn.parameter.Parameter(data=torch.rand(num_classes, num_classes))
 		self.origination_scores=nn.parameter.Parameter(data=torch.rand(num_classes, ))
 		self.batch_first=batch_first
+		self.num_classes=num_classes
 
 		return
 
@@ -46,8 +47,12 @@ class CRF(nn.Module):
 
 		return self._viterbi_decode(pad_x, batch_sizes)
 
-	def get_prob(self, x, y):
+	def score_tag_sequence(self, x, y):
+
 		"""
+		Returns a tensor of shape (batch_size, ) where the ith element denotes the probability
+		of y[i] being the optimal sequence tagging for x[i]
+
 		x: a packed sequence representing the following data
 		if batch_first==True:
 			x.data has shape batch_size x max_seq_len x num_classes (when padded)
@@ -60,9 +65,42 @@ class CRF(nn.Module):
 		else:
 			y.data has shape max_seq_len x batch_size (when padded)
 		y stores the integer class indices.
-
-		Return a tensor of shape (batch_size, ) where the ith element denotes the probability
-		of y[i] being the optimal sequence tagging for x[i]
 		"""
 
-		
+		pad_x, batch_sizes=pad_packed_sequence(x, batch_first=self.batch_first)
+		if not self.batch_first:
+			pad_x=torch.swapaxes(pad_x, 0, 1)
+
+		normalizer=self._forward_algo(pad_x, batch_sizes)
+
+		# TODO implement numerator
+
+	def _forward_algo(self, pad_x, batch_sizes):
+
+		num_samples, max_seq_len, _=pad_x.shape
+		alpha=torch.zeros(max_seq_len, num_samples, self.num_classes)
+
+		alpha[0]=pad_x[:, 0, :]+self.origination_scores.reshape((1, -1)).expand(num_samples, -1)
+
+		# Dont try to make sense of variable names for the next section
+		# Life is too short
+		# Dont try to make sense of anything in the next section, infact
+
+		for i in range(1, max_seq_len):
+
+			# expand does not allow expanding non-singleton dimensions
+			# So, introduce a dummy dimension and then expand
+			# Also, expand returns a view, so clone because we need to write to this
+
+			J=torch.clone(
+				alpha[i-1][:, None, :].expand(-1, self.num_classes, -1)
+				)
+
+			J=J+self.transition_scores
+
+			# J has shape N, M, M 
+			alpha[i]=pad_x[:, i, :]+torch.logsumexp(J, axis=2) # Review that reduction is on axis=2
+			
+
+
+
